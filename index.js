0f5c28d8ae5cd75e01bc011dc265b7f00f9a299a
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-12 16:54:47 -0700

    Made new database. added example student.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -15,27 +14,16 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/db', async function(req, res){
+      pool.query("SELECT * FROM students", function(error, result){
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+      });
+  });
 
-showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
-}
+/*app.get('/users/:id', function(req, res){
+  console.log(req.params.id);
+})*/
+
+
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
