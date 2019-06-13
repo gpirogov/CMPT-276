3da7c452d16a33aea9b8569264d790de35af2920
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-12 18:37:59 -0700

    running into annoying bugs

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
 
 const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+
+var pool = new Pool({
+  	connectionString: process.env.DATABASE_URL
+	/*user: 'postgres',
+   	host: 'localhost',
+   	password: 'root',
+   	database: 'postgres'*/
+
 });
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/users', function(req, res){
+	pool.query("INSERT INTO students(id, lName, fName, gender, ethnicity, hColour, fColour, weight, wType, fHeight, iHeight, gpa) VALUES(0, 'Clarke', 'Melissa', 'Female', 'Caucasian', 'Blonde', 'Brown', 165.0, 'lbs', 5, 10, 3.4)", (error, result) =>{
+		console.log(error, result);
+		pool.end();
+	});
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async function(req, res){
-      pool.query("SELECT * FROM students", function(error, result){
-        const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results );
-      });
+	pool.query("SELECT * FROM students", (error, result) =>{
+		console.log(error, result);
+		pool.end();
+	});
+});
+
+/*app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/students', async function(req, res){
+  pool.query("SELECT * FROM students", function(error, result){
+    var results = { 'results': (result.rows[0].id) ? result.rows : [] };
+    res.render('pages/db', results);
   });
 
-/*app.get('/users/:id', function(req, res){
+  console.log("test");
+});
+app.get('/students/:id', function(req, res){
   console.log(req.params.id);
-})*/
+});*/
 
 
-express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- public/js/students.js
+++ public/js/students.js
@@ -1,1 +1,3 @@
 // don't add db stuff here.
+
+//INSERT INTO students VALUES(0, 'Clarke', 'Melissa', 'Female', 'Caucasian', 'Blonde', 'Brown', 165.0, 'lbs', 5, 10, 3.4);
