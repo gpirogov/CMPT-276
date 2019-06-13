4f64383531d6cc2daa9a532f3570733025f5b5d0
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-13 16:18:25 -0700

    having trouble logging data. working on fixing that now

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());  
 
 app.get('/new-student', function(req, res){
-  res.render('form');
+  //res.render('form');
   res.sendFile("index.html"); //if html file is within public directory
 });
 
@@ -37,8 +37,9 @@ app.post('/new-student', function(req, res){
   pool.query(insertString);*/
   //res.redirect('/students.html');
   
-   var reqBody = req.body;
-   var htmlData = 'Hello:' + reqBody;
+   //var reqBody = req.body;
+   //var htmlData = 'Hello:' + reqBody;
+   var html = "test5";
    res.send(htmlData);
    console.log(htmlData);
 
