48cad046125dce94275a844ec274648a5949ca7c
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-12 20:59:57 -0700

    testing something

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
+
+
+
 app.post('/loginnn', function(req, res){
   pool.query('INSERT INTO students VALUES(123123)');
 });
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM students');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/db', function(req, res){
+  const result = pool.query('SELECT * FROM students');
+  const results = { 'results': (result) ? result.rows : null};
+  res.render('pages/db', results );
+});
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
