faebdedb13daa3efbbc88046a685f14b483e8450
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-12 21:02:52 -0700

    testing something

--- index.js
+++ index.js
@@ -18,20 +18,25 @@ app.post('/loginnn', function(req, res){
 
 
 
+
+
+//app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
 app.get('/db', async (req, res) => {
   try {
-    result = pool.query('SELECT * FROM students');
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM students');
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
+    client.release();
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
   }
-});
+})
 
 
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
