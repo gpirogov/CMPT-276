0b9737547dc7aa86e34501c264dbd86493826a75
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-13 22:45:55 -0700

    trying to get website to insert into table based on form data

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.post('/new-student', function(req, res){
   req.body.weight + ", " + "-1" + ", " + req.body.height + ", " + "-1" + ", " + req.body.gpa + ")";
   pool.query(insertString);
   
+  pool.query("INSERT INTO students VALUES(3)");
+
   var testString = "INSERT INTO students VALUES(2)";
   pool.query(testString);
   //res.redirect('/students.html');
