3d4939c0d41596e430b80485c0127d7fe9ee092d
Gleb Pirogov <gpirogov@sfu.ca>
2019-06-14 15:01:42 -0700

    trying to access table data from client side now. using ajax

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get('/db', async function(req, res){
 });
 
 
-app.get('students/:id'){
+app.get('students/:id', function(req,res) {
   console.log("test8");
   res.redirect('students.html');
 }
